225a4e02808d378ef97db7efd1c753e8bb80a949
rakeshkumarkaranam <krk423@gmail.com>
2022-02-02 12:20:21 +0530

    First draft

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() async {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // StatelessWidget is for static pages
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Hello World'),
+          backgroundColor: Colors.blue.shade200,
+        ),
+        body: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('Arise, Awake and stop not till the goal is reached'),
+            TextButton(
+              onPressed: () => print('Button is pressed'),
+              child: Text('Press for the next quote'),
+              style: TextButton.styleFrom(
+                  primary: Colors.white, backgroundColor: Colors.blue.shade100),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
